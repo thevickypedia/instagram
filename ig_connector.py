868263800c4e3ac5f096bc50e00ec17bc7abcfbe
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-04-12 10:13:59 -0500

    get profile picture saved locally

--- ig_connector.py
+++ ig_connector.py
@@ -1,6 +1,9 @@
 def instagram():
     import os
     from instaloader import Instaloader, Profile
+    import requests
+    import shutil
+    import subprocess
     client = Instaloader()
     insta_user, insta_pass = os.getenv('insta_user'), os.getenv('insta_pass')
     if not (insta_user or insta_pass):
@@ -19,8 +22,19 @@ def instagram():
             username = follower.username
             print(username)
 
+    def dp(target_profile):
+        profile = Profile.from_username(client.context, target_profile)  # use target user here to download the dp
+        filename = f'{target_profile}.jpg'
+        response = requests.get(profile.get_profile_pic_url(), stream=True)
+        response.raw.decode_content = True
+        with open(filename, 'wb') as file:
+            shutil.copyfileobj(response.raw, file)
+            file.close()
+        subprocess.call(["open", filename])
+
     followees()
     followers()
+    dp('vignesh.vikky')
 
 
 instagram()
