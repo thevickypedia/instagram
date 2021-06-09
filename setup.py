a59163ea3986ea8ff578606ad692da1f91b0bfeb
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-06-08 23:40:24 -0500

    uncomment accidental comment

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-# from setuptools import setup
-#
-# setup(
-#     entry_points={
-#         'console_scripts': [
-#             'instagram = ig_connector'
-#         ]
-#     }
-# )
+from setuptools import setup
+
+setup(
+    entry_points={
+        'console_scripts': [
+            'instagram = ig_connector'
+        ]
+    }
+)
